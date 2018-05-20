f6e294dcf85b6dca97fbea610e0fdbf85217dfef
fynv <feiy@nvidia.com>
2018-05-20 22:38:32 +0800

    Adding Oto file processing class

--- SingingGadgets/UTAUUtils.py
+++ SingingGadgets/UTAUUtils.py
@@ -1,1 +1,82 @@
 from .PyUTAUUtils import *
+
+class OtoMap:
+	def __init__(self):
+		self.oto_map={}
+
+	def AddOtoINIPath(self,path):
+		otoIniPath=path+'/oto.ini'
+		with open(otoIniPath,'r', encoding='mbcs') as f:
+			while True:
+				line = f.readline()
+				if not line:
+					break
+				p = line.find('=')
+				if p==-1:
+					continue
+				fn=line[0:p]
+
+				p+=1
+
+				# lyric
+				lyric=''
+				p2 = line.find(',',p)
+				if p2==-1:
+					continue
+				if p2>p:
+					lyric=line[p:p2]
+				p=p2+1
+
+				if len(lyric)==0:
+					lyric = fn[0:len(fn)-4]
+
+				# offset
+				offset=0
+				p2 = line.find(',',p)
+				if p2==-1:
+					continue
+				if p2>p:
+					offset=float(line[p:p2])
+				p=p2+1
+
+				# consonant
+				consonant = 0
+				p2 = line.find(',',p)
+				if p2==-1:
+					continue
+				if p2>p:
+					consonant=float(line[p:p2])
+				p=p2+1
+
+				# cutoff
+				cutoff = 0
+				p2 = line.find(',',p)
+				if p2==-1:
+					continue
+				if p2>p:
+					cutoff=float(line[p:p2])
+				p=p2+1
+
+				# preutter
+				preutterance = 0
+				p2 = line.find(',',p)
+				if p2==-1:
+					continue
+				if p2>p:
+					preutterance=float(line[p:p2])
+				p=p2+1
+
+				# overlap
+				overlap = float(line[p:])
+
+				properties={
+					'filename': path+'/'+fn,
+					'offset': offset,
+					'consonant': consonant,
+					'cutoff': cutoff,
+					'preutterance': preutterance,
+					'overlap': overlap
+				}
+
+				self.oto_map[lyric]=properties
+
--- SingingGadgets/__init__.py
+++ SingingGadgets/__init__.py
@@ -1,4 +1,5 @@
 from .UTAUUtils import LoadFrq as LoadFrqUTAU
+from .UTAUUtils import OtoMap as OtoMapUTAU
 from .VoiceSampler import S16ToF32 as S16ToF32Voice
 from .VoiceSampler import F32ToS16 as F32ToS16Voice
 from .VoiceSampler import MaxValueF32 as MaxValueF32Voice
